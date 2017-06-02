1d3f1081b25d315c0908297f63643b12652fd6eb
Zetttta <parkgh0921@gmail.com>
2017-06-02 11:24:20 +0900

    header, css ユーザ情報出力のため修正

--- StaticProject/header.php
+++ StaticProject/header.php
@@ -8,15 +8,20 @@
  作成日：        2017/05/18/14:00
  最終更新日：    2017/05/18/17:00
  レビュー担当者：
- レビュー日：    
- バージョン：    
- 
- 
- ロゴタイプの代用画像作成はhttp://placehold.jp/を参考にしました
- 
- 
--->
+ レビュー日：
+ バージョン：
 
+
+ ロゴタイプの代用画像作成はhttp://placehold.jp/を参考にしました
+
+
+-->
+<?php
+  // 管理者か否かで管理者用画面の表示を切り替える
+  // 管理者のときのみ表示
+  require_once('function/database_session.php');
+  require_once('function/xss.php');
+  ?>
 <!-- ヘッダーを宣言 -->
 <div id="header">
   <!-- 会社のロゴタイプ -->
@@ -25,26 +30,134 @@
     <a href="/static_project/php/toppage/toppage.php">
       <img src="/static_project/img/alegria.png" height="100px" width="300px">
     </a>
+    <div id="user_info">
+      <table>
+      <?php
+      // マイページ内容書き出し処理。名前込み
+      $dbh = connectDb();
+      try {
+          // SQLを構築
+          $sql = "SELECT * FROM m_user";
+          $sql .= " WHERE user_id = :user";//誰のマイページを開くのか。
+          $sth = $dbh->prepare($sql); // SQLを準備
+          $sth->bindValue(":user", "{$_GET['user_id']}");
+          // SQLを発行
+          $sth->execute();
+
+      } catch (PDOException $e) {
+          exit("SQL発行エラー：{$e->getMessage()}");
+      }
+    ?>
+        <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {
+          $username = $row["user_name"];
+          $userid = $row["user_id"];
+          $storeid = $row["store_id"];
+          $postid = $row["post_id"];
+          $cornerid = $row["corner_id"];
+          $userright = $row["user_right"];
+          $profile = $row["user_profile"];
+
+        }?>
+        <tr>
+            <th>名前</th>
+            <td><?php ph($username);?></td>
+        </tr>
+        <tr>
+        <?php
+          // 所属店舗書き出し
+          $dbh = connectDb();
+          try {
+              // SQLを構築
+              $sql = "SELECT * FROM m_store WHERE store_id = $storeid";//ユーザテーブルでとったストアID
+              $sth = $dbh->prepare($sql); // SQLを準備
+
+              // SQLを発行
+              $sth->execute();
+
+          } catch (PDOException $e) {
+              exit("SQL発行エラー：{$e->getMessage()}");
+          }
+        ?>
+            <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {?>
+            <th>所属</th>
+            <td><?php ph($row["store_name"]);?></td>
+            <?php } ?>
+        </tr>
+        <tr>
+        <?php
+          // 役職書き出し
+          $dbh = connectDb();
+          try {
+              // SQLを構築
+              $sql = "SELECT * FROM m_post WHERE post_id = $postid";//ユーザテーブルでとったポストID
+              $sth = $dbh->prepare($sql); // SQLを準備
+
+              // SQLを発行
+              $sth->execute();
+
+          } catch (PDOException $e) {
+              exit("SQL発行エラー：{$e->getMessage()}");
+          }
+        ?>
+            <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {?>
+            <th>役職</th>
+            <td><?php ph($row["post_name"]);?></td>
+            <?php } ?>
+        </tr>
+        <tr>
+        <?php
+          // 売場書き出し
+          $dbh = connectDb();
+          try {
+              // SQLを構築
+              $sql = "SELECT * FROM m_corner WHERE corner_id = $cornerid";//ユーザテーブルでとったポストID
+              $sth = $dbh->prepare($sql); // SQLを準備
+
+              // SQLを発行
+              $sth->execute();
+
+          } catch (PDOException $e) {
+              exit("SQL発行エラー：{$e->getMessage()}");
+          }
+        ?>
+            <?php while ($row = $sth->fetch(PDO::FETCH_ASSOC)) {?>
+            <th>売場</th>
+            <td><?php ph($row["corner_name"]);?></td>
+            <?php } ?>
+        </tr>
+        <tr>
+          <!--資格書き出し-->
+            <th>資格</th>
+            <td><?php ph($userright);?></td>
+        </tr>
+        <tr>
+          <!--コメント書き出し-->
+            <th>コメント</th>
+            <td><?php if(isset($profile)) {
+                         ph($profile);
+                      } else { ?>
+                        無し
+               <?php } ?></td>
+        </tr>
+    </table>
+    </div>
   </div>
   <!-- メニュー -->
   <div id="headMenu" class="menuRange">
     <!-- それぞれがそれぞれのページへの遷移ボタン -->
-    
+
     <a href="/static_project/php/login/logout.php"><div id="headMenuLogout">ログアウト</div></a>
     <!-- php及びsqlを挟む、SESSIONのidentifyのユーザーIDが管理者IDである場合のみ書き出し処理を行う -->
     <?php
-      // 管理者か否かで管理者用画面の表示を切り替える
-      // 管理者のときのみ表示
-      require_once('function/database_session.php');
       // ユーザー情報
       $Identify = getUser();
       // 管理者フラグ取得
       $judge = getManager($Identify);
       if($judge == 1){
-        echo '<a href="/static_project/php/management/management.php"><div id="headMenuManagement">管理者用画面</div></a>';
+        echo '<a href="/static_project/php/management/management.php?"><div id="headMenuManagement">管理者用画面</div></a>';
       }
     ?>
-    <?php 
+    <?php
       echo '<a href="/static_project/php/mypage/mypage_main.php?user_id=' . "{$Identify}" . '">';
     ?>
       <div id="headMenuMypage">マイページ</div></a>
